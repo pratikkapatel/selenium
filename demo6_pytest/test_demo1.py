8a7367a5ecb3a6deda4b9c9f7fe1bfb76c30e26c
pratik <pratik.kapatel@einfochips.com>
2023-04-01 16:17:13 +0530

    fluent wait

--- demo6_pytest/test_demo1.py
+++ demo6_pytest/test_demo1.py
@@ -2,3 +2,9 @@ import pytest
 
 def test_valid_login():
     print("valid login!!")
+
+def test_invalid():
+    print("invalid")
+    actual_title=''
+    assert actual_title=='OrangeHRM'
+    assert 'Orange' in actual_title
